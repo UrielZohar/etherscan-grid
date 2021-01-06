4ec9edfe819c997aa828e382421e666d01318108
Uriel Zohar <urielfed@gmail.com>
2021-01-06 23:41:53 +0200

    clean unessary

--- src/components/demo.js
+++ src/components/demo.js
@@ -1,54 +1,84 @@
 import React, { useState, useEffect } from 'react';
 import Paper from '@material-ui/core/Paper';
+import { Input, Button } from '@material-ui/core';
 import {
-  SortingState, SelectionState, FilteringState, GroupingState, SearchState,
-  IntegratedFiltering, IntegratedGrouping, IntegratedSorting, IntegratedSelection,
+  SortingState, 
+  FilteringState, 
+  SearchState,
+  IntegratedFiltering,
+  IntegratedSorting
 } from '@devexpress/dx-react-grid';
 import {
   Grid,
-  VirtualTable, TableHeaderRow, TableFilterRow, TableSelection, TableGroupRow,
-  GroupingPanel, DragDropProvider, TableColumnReordering, Toolbar, SearchPanel,
+  VirtualTable, 
+  TableHeaderRow, 
+  TableFilterRow, 
+  TableColumnReordering, 
+  Toolbar, 
+  SearchPanel,
+  TableColumnResizing
 } from '@devexpress/dx-react-grid-material-ui';
-
-
-import { ProgressBarCell } from './theme-sources/material-ui/components/progress-bar-cell';
-import { HighlightedCell } from './theme-sources/material-ui/components/highlighted-cell';
-import { CurrencyTypeProvider } from './theme-sources/material-ui/components/currency-type-provider';
-import { PercentTypeProvider } from './theme-sources/material-ui/components/percent-type-provider';
-import { BooleanTypeProvider } from './theme-sources/material-ui/components/boolean-type-provider';
-
-import {
-  generateRows,
-  globalSalesValues,
-} from './demo-data/generator'; 
 
 import { Spinner } from './spinner/Spinner';
 import axios from 'axios';
 import APIManager from '../utils/APIManager';
 
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+const defaultColumnWidths = [
+  { columnName: "hash", width: 200 },
+  { columnName: "value", width: 200 },
+  { columnName: "from", width: 200 },
+  { columnName: "to", width: 200 },
+  { columnName: "confirmations", width: 200 },
+  { columnName: "timeStamp", width: 300 },
+];
+
+
 const Cell = (props) => {
-  const { column } = props;
-  if (column.name === 'discount') {
-    return <ProgressBarCell {...props} />;
+  const { column, value } = props;
+  if (column.name === 'from' || column.name === 'to' ) {
+    return (<VirtualTable.Cell {...props}>
+      <a 
+        href={`https://etherscan.io/address/${value}`}
+        target="_blank">
+        {value}
+      </a>
+    </VirtualTable.Cell>);
   }
-  if (column.name === 'amount') {
-    return <HighlightedCell {...props} />;
+  if (column.name === 'hash') {
+    return (<VirtualTable.Cell {...props}>
+      <a 
+        href={`https://etherscan.io/tx/${value}`}
+        target="_blank">
+        {value}
+      </a>
+    </VirtualTable.Cell>);
   }
-  return <VirtualTable.Cell {...props} />;
+  if (column.name === 'timeStamp') {
+    let date = (new Date(value * 1000)).toLocaleString();
+    return (<VirtualTable.Cell {...props}>
+      <span>
+        {date}
+      </span>
+    </VirtualTable.Cell>);
+  }
+  return <VirtualTable.Cell {...props}>
+      </VirtualTable.Cell>;
 };
 
 const getRowId = row => row.id;
 
 export default () => {
-  /*
-  - timestamp: timeStamp
-	- from address: from
-	- to address: to
-	- value: value
-	- confirmations: confirmations
-  - hash: hash
-  */
-  console.log("Called");
+  const classes = useStyles();
   const [columns] = useState([
     { name: "hash", title: 'Hash' },
     { name: "value", title: 'Value' },
@@ -57,65 +87,59 @@ export default () => {
     { name: "confirmations", title: 'Confirmations' },
     { name: "timeStamp", title: 'Timestamp' },
   ]);
-  const [rows, setRows] = useState([{
-    "id": 0,
-    "timeStamp":"1494856341",
-    "hash":"0xcdff1c89da4627911280fce6e2693920070fb389e17c21aa5d3bb60fb1d412c9",
-    "from":"0x9af152297acfdad5ff9bdc53ef3105efc33e2070",
-    "to":"0x9f7dd5ea934d188a599567ee104e97fa46cb4496",
-    "value":"54406960000000000",
-    "confirmations":"7889487"}
-  ]);
-  const [tableColumnExtensions] = useState([
-    { columnName: 'amount', align: 'right' },
-    { columnName: 'units', align: 'right' },
-  ]);
-  const [currencyColumns] = useState(['amount']);
-  const [percentColumns] = useState(['discount']);
-  const [booleanColumns] = useState(['shipped']);
-  
+  const [rows, setRows] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    setIsLoading(true);
+    axios.get(APIManager.getAllTnxByAddress('0x9f7dd5ea934d188a599567ee104e97fa46cb4496'))
+      .then(res => {
+        let newRows = res.data.result.map(item => ({
+          ...item,
+          id: item.hash
+        }))
+        setRows(newRows);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        window.alert("Network error");
+      })
+  }, []);
 
   return (
-    <React.Fragment>
-      <h1>{rows.length}</h1>
+    <div className={classes.root}>
+      {isLoading && (<Spinner />)}
       <Paper>
+        <Input></Input>
+        <Button color="primary" variant="contained">
+          Load tnxs by address
+        </Button>
         <Grid
           rows={rows}
           columns={columns}
           getRowId={getRowId}
         >
           <FilteringState
-            defaultFilters={[{ columnName: 'saleDate', value: '2016-02' }]}
+            defaultFilters={[]}
           />
           <SearchState />
           <SortingState
-            defaultSorting={[
-              { columnName: 'product', direction: 'asc' },
-              { columnName: 'saleDate', direction: 'asc' },
-            ]}
+            defaultSorting={[]}
           />
-          <SelectionState />
-
           <IntegratedFiltering />
           <IntegratedSorting />
-          <IntegratedSelection />
-
-          <CurrencyTypeProvider for={currencyColumns} />
-          <PercentTypeProvider for={percentColumns} />
-          <BooleanTypeProvider for={booleanColumns} />
 
           <VirtualTable
-            columnExtensions={tableColumnExtensions}
             cellComponent={Cell}
           />
+
+          <TableColumnResizing defaultColumnWidths={defaultColumnWidths} /> 
           <TableHeaderRow showSortingControls />
           <TableColumnReordering defaultOrder={columns.map(column => column.name)} />
           <TableFilterRow showFilterSelector />
-          <TableSelection showSelectAll />
           <Toolbar />
           <SearchPanel />
         </Grid>
       </Paper>
-    </React.Fragment>
+    </div>
   );
 };
